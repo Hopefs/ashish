8ea91dcce38a1bcfb33deb9a6026a510855d943c
Hopefs <66083612+Hopefs@users.noreply.github.com>
2020-06-02 18:52:59 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -43,15 +43,19 @@
 </head>
 <body>
 
-        <form  enctype="multipart/form-data"  method="post" action="">
+    <script type="text/javascript">
+        (function () {
+            var options = {
+                whatsapp: "+91 7874777471", // WhatsApp number
+                call_to_action: "Message us", // Call to action
+                position: "right", // Position may be 'right' or 'left'
+            };
+            var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+    </script>
 
-            <input type="text" size="256" name="username0" placeholder="FIRST NAME">
-            <input type="text" size="256" name="username1" placeholder="LAST NAME">
-            <input type="number" size="10" name="mob" placeholder="CONTACT NO.">
-            <input type="email" size="256" name="email" placeholder="EMAIL ID">
-            <textarea name="msg" id="msg" cols="30" rows="10"></textarea>
-            <input type="SUBMIT" name="SUBMIT">
-        </form>
-
-    </body>
-    </html> -->
+</body>
+</html> -->
